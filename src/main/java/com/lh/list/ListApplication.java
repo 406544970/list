f39b89cdb4f1c5a7100c515936b2cdd77c0cbe16
flymanflyman <406544970@qq.com>
2019-03-30 23:27:01 +0800

    30.1

--- src/main/java/com/lh/list/ListApplication.java
+++ src/main/java/com/lh/list/ListApplication.java
@@ -9,6 +9,7 @@ public class ListApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ListApplication.class, args);
 		System.out.println("The is Universal demo of list!");
+		System.out.println("The writer is liang hao.");
 	}
 
 }
