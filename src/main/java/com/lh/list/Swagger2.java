9325b9a5378319a7384845abe8954557ac879115
flymanflyman <406544970@qq.com>
2019-03-31 22:01:42 +0800

    31.1

---
+++ src/main/java/com/lh/list/Swagger2.java
@@ -0,0 +1,43 @@
+package com.lh.list;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+/**
+ * @author 梁昊
+ * @date 2019/3/31
+ * @function
+ * @editLog
+ */
+@Configuration
+@EnableSwagger2
+public class Swagger2 {
+    @Bean
+    public Docket createRestApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.lh.list"))
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("Java List 常规用法演示")
+                .description("包括：group/having/sort/left join/inner join等")
+                .termsOfServiceUrl("http://www.baidu.com/")
+                .version("1.1")
+                .contact(new Contact("梁昊",null,"406544970@qq.com"))
+                .build();
+    }
+
+}
--- src/main/java/com/lh/list/controller/UseController.java
+++ src/main/java/com/lh/list/controller/UseController.java
@@ -2,7 +2,12 @@ package com.lh.list.controller;
 
 import com.lh.list.model.SexHaving;
 import com.lh.list.model.User;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import unit.PageList;
 import unit.SortList;
@@ -17,6 +22,8 @@ import java.util.stream.Collectors;
  * @editLog
  */
 @RestController
+@RequestMapping(value = "/users")     // 通过这里配置使下面的映射都在/users下，可去除
+@Api(value = "用户控制类", description = "用户控制类")
 public class UseController {
     private List<User> userList;
 
@@ -42,6 +49,7 @@ public class UseController {
     /**
      * @return 返回列表数量
      */
+    @ApiOperation(value = "获取用户列表", notes = "哈哈哈")
     @PostMapping("/getListCount")
     public int getListCount() {
         return this.userList.size();
@@ -54,6 +62,10 @@ public class UseController {
      * @param ascSign true:升，false:降
      * @return 列表
      */
+    @ApiOperation(value = "根据标识，按年龄排序", notes = "true:升序、false:降序")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "ascSign", value = "升序或降序", required = true, dataType = "Boolean")
+    })
     @PostMapping("/getListSortByAge")
     public List<User> getUserSortByAge(Boolean ascSign) {
         SortList<User> sortList = new SortList<>();
@@ -72,6 +84,11 @@ public class UseController {
      * @param endIndex   终点
      * @return 子列表
      */
+    @ApiOperation(value = "根据起点至终点，得到子列表", notes = "根据起点至终点，得到子列表")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "beginIndex", value = "开始数", required = true, dataType = "int"),
+            @ApiImplicitParam(name = "endIndex", value = "结束数", required = true, dataType = "int")
+    })
     @PostMapping("/getSubList")
     public List<User> getSubList(int beginIndex, int endIndex) {
         if (beginIndex < 0)
@@ -87,9 +104,13 @@ public class UseController {
      * @param index 列表序号
      * @return 对应的内容
      */
+    @ApiOperation(value = "根据列表序号，得到内容", notes = "根据列表序号，得到内容")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "index", value = "序号", required = true, dataType = "int")
+    })
     @PostMapping("/getValue")
     public User getValue(int index) {
-        if (index > -1) {
+        if (index > -1 && index < userList.size()) {
             return userList.get(index);
         } else {
             return null;
@@ -101,6 +122,7 @@ public class UseController {
      *
      * @return 仅返回两种性别的信息
      */
+    @ApiOperation(value = "根据性别去掉重复", notes = "根据性别去掉重复")
     @PostMapping
     public List<User> getDeleteSame() {
         List<User> listTemp = new ArrayList();
@@ -123,6 +145,11 @@ public class UseController {
      * @param pageSize  每页条数
      * @return 列表
      */
+    @ApiOperation(value = "得到分页的列表", notes = "得到分页的列表")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "pageIndex", value = "当前页数", required = true, dataType = "int"),
+            @ApiImplicitParam(name = "pageSize", value = "每页条数", required = true, dataType = "int")
+    })
     @PostMapping("/getPageList")
     public List<User> getPageList(int pageIndex, int pageSize) {
         return (List<User>) PageList.getPageList(pageIndex, pageSize, userList);
@@ -133,6 +160,7 @@ public class UseController {
      *
      * @return 返回Key：性别，Value：分组内容
      */
+    @ApiOperation(value = "根据性别，分组", notes = "根据性别，分组")
     @PostMapping("/getListGroupBySex")
     public Map<Boolean, List<User>> getListGroupBySex() {
         Map<Boolean, List<User>> sexMap = userList.stream()
@@ -146,6 +174,10 @@ public class UseController {
      * @param havingCount 数量限制
      * @return 根据性别，统计人数，返回HashMap
      */
+    @ApiOperation(value = "根据性别，统计人数", notes = "根据性别，统计人数", response = Map.class)
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "havingCount", value = "数量限制", required = true, dataType = "int")
+    })
     @PostMapping("/getMapGroupByHavingSex")
     public Map<Boolean, Integer> getMapGroupByHavingSex(int havingCount) {
         Map<Boolean, List<User>> sexMap = userList.stream()
@@ -166,13 +198,17 @@ public class UseController {
      * @param havingCount 数量限制
      * @return 根据性别，统计人数，返回list
      */
+    @ApiOperation(value = "根据性别，统计人数", notes = "根据性别，统计人数", response = List.class)
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "havingCount", value = "数量限制", required = true, dataType = "int")
+    })
     @PostMapping("/getListGroupByHavingSex")
     public List<SexHaving> getListGroupByHavingSex(int havingCount) {
         List<SexHaving> sexHavingList = new ArrayList<>();
         Map<Boolean, List<User>> sexMap = userList.stream()
                 .collect(Collectors.groupingBy(User::getSex));
         for (Map.Entry<Boolean, List<User>> entry : sexMap.entrySet()) {
-            if (entry.getValue().size() > havingCount){
+            if (entry.getValue().size() > havingCount) {
                 SexHaving sexHaving = new SexHaving();
                 sexHaving.setSex(entry.getKey());
                 sexHaving.setCount(entry.getValue().size());
@@ -188,6 +224,7 @@ public class UseController {
      *
      * @return 姓名集合，而且去掉了重复
      */
+    @ApiOperation(value = "得到姓名列表", notes = "得到姓名列表",response = List.class)
     @PostMapping("/getUserNameList")
     public List<String> getUserNameList() {
         Map<String, List<User>> nameMap = userList.stream()
